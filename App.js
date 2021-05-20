b6c67f32490468eaa70cf32342a3f5b1460a3776
Poppy Ng <mmiswaiting@gmail.com>
2021-05-20 18:02:56 -0400

    redesign the ui

--- App.js
+++ App.js
@@ -1,33 +1,61 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { Home, Score } from "./pages/index";
+import { AnimatedTabBarNavigator } from "react-native-animated-nav-tab-bar";
+import Icon from "react-native-vector-icons/Feather";
 import { Provider } from "react-redux";
-import { store } from "./store";
-import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
-import CardList from "./component/CardList";
-import AddTask from "./component/AddTask";
+import store from "./redux/store";
 
-export default function App() {
-  return (
-    <Provider store={store}>
-      <SafeAreaView>
-        <View style={styles.container}>
-          <Text>Achieved</Text>
-          <CardList />
-          <View>
-            <Button style={styles.button} title="add" />
-          </View>
-          <StatusBar style="auto" />
-          <AddTask />
-        </View>
-      </SafeAreaView>
-    </Provider>
-  );
+const Tab = AnimatedTabBarNavigator();
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <Tab.Navigator
+            tabBarOptions={{
+              activeColor: 'c8c2bc',
+              inactiveTintColor: "grey",
+            }}
+          >
+            <Tab.Screen
+              name="Home"
+              component={Home}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Icon
+                    name="home"
+                    size={size ? size : 24}
+                    color={focused ? color : "#000"}
+                    focused={focused}
+                    color={color}
+                  />
+                )
+              }}
+            />
+            <Tab.Screen
+              name="Score"
+              component={Score}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Icon
+                    name="activity"
+                    size={size ? size : 24}
+                    color={focused ? color : "#000"}
+                    focused={focused}
+                    color={color}
+                  />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </Provider>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
+const styles = StyleSheet.create({});
