3c8a47ca514bc66986f90341186ca68bb6f23a1d
Poppy Ng <mmiswaiting@gmail.com>
2021-05-20 18:18:31 -0400

    remove extra files

--- store.js
+++
@@ -1,7 +0,0 @@
-import {createStore,combineReducers,applyMiddleware} from 'redux';
-import {tasksReducer} from './component/tasksSlice';
-
-const reducers={
-    tasks:tasksReducer
-};
-export const store=createStore(combineReducers(reducers));
